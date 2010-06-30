1a2d547b379ce70106812ec49d3ed7298c727d09
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2010-06-30 15:58:12 -0500

    Coding style changes.

--- src/jaguar/structures/jstructures/JState.java
+++ src/jaguar/structures/jstructures/JState.java
@@ -1,26 +1,26 @@
 /**
 ** <JState.java> -- The State's graphical extension
-** 
+**
 ** Copyright (C) 2002 by  Ivan Hernández Serrano
 **
 ** This file is part of JAGUAR
-** 
+**
 ** This program is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU General Public License
 ** as published by the Free Software Foundation; either version 2
 ** of the License, or (at your option) any later version.
-** 
+**
 ** This program is distributed in the hope that it will be useful,
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-** 
+**
 ** Author: Ivan Hernández Serrano <ivanx@users.sourceforge.net>
-** 
+**
 **/
 
 
@@ -44,23 +44,21 @@ import java.lang.Math;
  * @version $Revision: 1.1 $ $Date: 2005/01/31 19:25:05 $
  */
 
-public class JState extends jaguar.structures.State{
+public class JState extends jaguar.structures.State {
+
 
     /**
-     ** El diametro del circulo circunscrito que identificará gráficamente a un estado inicial
-     **/
+     * El diametro del circulo circunscrito que identificará gráficamente a un estado inicial
+     */
     public static final double DIAMETRO_INITIAL_STATE = 15;
 
     /**
      * El diametro de un JState, son graficos 50 pixels
-     **/
-    public static final double DIAMETRO = 50;
-    /**
-     *El radio de un JState
      */
-    public static final double RADIO  = Math.sqrt(50);
+    public static final double DIAMETRO = 50;
+
     /**
-     *  Elipse 2D que usamos para el chequeo de puntos dentro de un JState, la usamos para el drag & drop, 
+     *  Elipse 2D que usamos para el chequeo de puntos dentro de un JState, la usamos para el drag & drop,
      *  para los tooltips, y claro, para dibujar los estados
      **/
     private Ellipse2D e2d;
@@ -75,40 +73,39 @@ public class JState extends jaguar.structures.State{
      * @return el valor actual de circuloCinscunscritoEstadoInicial
      * @see #circuloCinscunscritoEstadoInicial
      */
-    protected Ellipse2D getCirculoCinscunscritoEstadoInicial(){
-	return circuloCinscunscritoEstadoInicial;
+    protected Ellipse2D getCirculoCinscunscritoEstadoInicial() {
+        return circuloCinscunscritoEstadoInicial;
     }
     /**
      * funcion de acceso para modificar circuloCinscunscritoEstadoInicial
      * @param new_circuloCinscunscritoEstadoInicial el nuevo valor para circuloCinscunscritoEstadoInicial
      * @see #circuloCinscunscritoEstadoInicial
      */
-    protected void setCirculoCinscunscritoEstadoInicial(Ellipse2D new_circuloCinscunscritoEstadoInicial){
-	circuloCinscunscritoEstadoInicial=new_circuloCinscunscritoEstadoInicial;
+    protected void setCirculoCinscunscritoEstadoInicial(Ellipse2D new_circuloCinscunscritoEstadoInicial) {
+        circuloCinscunscritoEstadoInicial=new_circuloCinscunscritoEstadoInicial;
     }
 
     /**
      * El punto dode se localiza en el componente gráfico
      **/
     protected Point location;
-    
+
     /**
      * Get the value of location.
      * @return value of location.
      */
     public Point getLocation() {
-	return location;
+        return location;
     }
-    
+
     /**
      * Set the value of location.
      * @param p Value to assign to location.
      */
     public void setLocation(Point  p) {
-	location = p;
-	updateEllipse2D();
+        location = p;
+        updateEllipse2D();
     }
-
 
     /**
      * Set the value of location.
@@ -116,49 +113,49 @@ public class JState extends jaguar.structures.State{
      * @param y the y coordinate
      */
     public void setLocation(double x, double y) {
-	location.setLocation(x,y);
-	updateEllipse2D();
+        location.setLocation(x,y);
+        updateEllipse2D();
     }
 
 
     protected static double  ZOOM_FACTOR;
-    
+
     /**
-       * Get the value of ZOOM_FACTOR.
-       * @return Value of ZOOM_FACTOR.
-       */
+     * Get the value of ZOOM_FACTOR.
+     * @return Value of ZOOM_FACTOR.
+     */
     public double getZOOM_FACTOR() {return ZOOM_FACTOR;}
-    
+
     /**
-       * Set the value of ZOOM_FACTOR.
-       * @param v  Value to assign to ZOOM_FACTOR.
-       */
+     * Set the value of ZOOM_FACTOR.
+     * @param v  Value to assign to ZOOM_FACTOR.
+     */
     public void setZOOM_FACTOR(double  v) {this.ZOOM_FACTOR = v;}
-    
+
     private void updateEllipse2D(){
-	e2d = new Ellipse2D.Double(getX(),getY(),DIAMETRO,DIAMETRO);
-	circuloCinscunscritoEstadoInicial = new Ellipse2D.Double(getX()+5+DIAMETRO/4,getY()+5+DIAMETRO/4,
-								 DIAMETRO_INITIAL_STATE,DIAMETRO_INITIAL_STATE);
+        e2d = new Ellipse2D.Double(getX(),getY(),DIAMETRO,DIAMETRO);
+        circuloCinscunscritoEstadoInicial = new Ellipse2D.Double(getX()+5+DIAMETRO/4,getY()+5+DIAMETRO/4,
+                                                        DIAMETRO_INITIAL_STATE,DIAMETRO_INITIAL_STATE);
     }
 
     protected Ellipse2D getEllipse(){
-	return e2d;	
+        return e2d;
     }
-    
+
     /**
      * Get the value of x in the <code>location</code> point
      * @return value of x.
      */
     public double getX() {
-	return(double) ((Point2D)location).getX();
+        return(double) ((Point2D)location).getX();
     }
-    
+
     /**
      * Set the value of x in the <code>location</code> point
      * @param x  Value to assign to the x coordinate
      */
     public void setX(double  x) {
-	location.setLocation(x,getY());
+        location.setLocation(x,getY());
     }
 
     /**
@@ -166,16 +163,16 @@ public class JState extends jaguar.structures.State{
      * @return value of y.
      */
     public double getY() {
-	return(double) ((Point2D)location).getY();
+        return(double) ((Point2D)location).getY();
     }
-    
+
     /**
      * Set the value of y in the <code>location</code> point
      * @param y  Value to assign to the y coordinate
      */
     public void setY(double  y) {
-	location.setLocation(getX(),y);
-    }        
+        location.setLocation(getX(),y);
+    }
 
     /**
      * El color por default es orange
@@ -187,7 +184,7 @@ public class JState extends jaguar.structures.State{
      * El color por default es orange
      **/
     public static final Color DEFAULT_FINAL_BG_COLOR = new Color(255, 102,0);// Color.orange;
-    
+
     /**
      * El color por default es orange
      **/
@@ -206,44 +203,44 @@ public class JState extends jaguar.structures.State{
      * El color para pintar el centro de un  estado inicial
      **/
     public static final Color DEFAULT_POINT_INITIAL_STATE = new Color(252,235,5);
-    
+
     /**
      * El color por default es orange
      **/
     private Color c = DEFAULT_BG_COLOR;
-    
+
     /**
-     * Get the value of c, the color of this state 
+     * Get the value of c, the color of this state
      * @return value of c.
      */
     public Color getC() {
-	return c;
+        return c;
     }
-    
+
     /**
      * Set the value of c.
      * @param _c  Value to assign to c.
      */
     public void setC(Color  _c) {
-	c = _c;
+        c = _c;
     }
 
-    private JState(){
-	this("",false);
+    private JState() {
+        this("",false);
     }
 
-    public JState(State st){
-	this(st.getLabel(), st.getIsInF());	
+    public JState(State st) {
+        this(st.getLabel(), st.getIsInF());
     }
 
-    
+
     /**
      * Constructora que recibe la etiqueta del estado, con esta
      * constructora el punto del estado  es el (0,0) y no es estado final, es equivalente a usar <code> JState(label,false)</code>
      *@param label La etiquta del estado
      **/
-    public JState(String label){
-	this(label,false);
+    public JState(String label) {
+        this(label,false);
     }
 
     /**
@@ -252,8 +249,8 @@ public class JState extends jaguar.structures.State{
      *@param label La etiquta del estado
      *@param _isInF verdadero si es un estado final, falso si no lo es
      **/
-    public JState(String label, boolean _isInF){
-	this(label,0,0, _isInF);
+    public JState(String label, boolean _isInF) {
+        this(label,0,0, _isInF);
     }
 
     /**
@@ -262,8 +259,8 @@ public class JState extends jaguar.structures.State{
      *@param x posicion sobre el eje X
      *@param y posicion sobre el eje Y
      **/
-    public JState(String label, double x, double y){
-	this(label,x,y,false);
+    public JState(String label, double x, double y) {
+        this(label,x,y,false);
     }
 
     /**
@@ -273,22 +270,22 @@ public class JState extends jaguar.structures.State{
      *@param y posicion sobre el eje Y
      *@param _isInF verdadero si es un estado final, falso si no lo es
      **/
-    public JState(String label, double x, double y, boolean _isInF){
-	super(label,_isInF);
-	location = new Point();
-	setLocation(x,y);
-	e2d = new Ellipse2D.Double(getX(),getY(),DIAMETRO,DIAMETRO);
-	circuloCinscunscritoEstadoInicial= new Ellipse2D.Double(getX()+5+DIAMETRO/4,getY()+5+DIAMETRO/4,
-								DIAMETRO_INITIAL_STATE,DIAMETRO_INITIAL_STATE);
+    public JState(String label, double x, double y, boolean _isInF) {
+        super(label,_isInF);
+        location = new Point();
+        setLocation(x,y);
+        e2d = new Ellipse2D.Double(getX(),getY(),DIAMETRO,DIAMETRO);
+        circuloCinscunscritoEstadoInicial= new Ellipse2D.Double(getX()+5+DIAMETRO/4,getY()+5+DIAMETRO/4,
+                                                        DIAMETRO_INITIAL_STATE,DIAMETRO_INITIAL_STATE);
     }
-    
+
 
     public String toString() {
-	return super.toString();
+        return super.toString();
     }
 
     /**
-     * Este nos dice si es un estado inicial para identificarlo gráficamente 
+     * Este nos dice si es un estado inicial para identificarlo gráficamente
      */
     protected boolean esEstadoInicial;
     /**
@@ -300,8 +297,8 @@ public class JState extends jaguar.structures.State{
      * @return el valor actual de esEstadoInicial
      * @see #esEstadoInicial
      */
-    public boolean getEsEstadoInicial(){
-	return esEstadoInicial;
+    public boolean getEsEstadoInicial() {
+        return esEstadoInicial;
     }
     /**
      * funcion de acceso para modificar esEstadoInicial
@@ -309,7 +306,7 @@ public class JState extends jaguar.structures.State{
      * @see #esEstadoInicial
      */
     public void setEsEstadoInicial(boolean new_esEstadoInicial){
-	esEstadoInicial=new_esEstadoInicial;
+        esEstadoInicial=new_esEstadoInicial;
     }
 
     /**
@@ -319,91 +316,91 @@ public class JState extends jaguar.structures.State{
      * @param esEstadoActual si es verdadero lo hace levantado, si es falso lo dibuja aplastado
      */
     public void paint(Graphics g, boolean esEstadoActual){
-	Graphics2D g2d = (Graphics2D) g;
-	Stroke origS = g2d.getStroke();
-	Color c;	
-	g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-			    RenderingHints.VALUE_ANTIALIAS_ON);
+        Graphics2D g2d = (Graphics2D) g;
+        Stroke origS = g2d.getStroke();
+        Color c;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                            RenderingHints.VALUE_ANTIALIAS_ON);
 
-	g.setColor(DEFAULT_BG_COLOR);
-	if(esEstadoActual==DEFAULT_CURRENT_STATE)// Si no lo quieren raise entonces lo quieren m'as obscuro
-	    g.setColor(DEFAULT_BG_CURRENT_STATE_COLOR);
-	g2d.fill(getEllipse());	
-	if(getIsInF()){
-	    g.setColor(DEFAULT_FINAL_BG_COLOR);
-	    g2d.setStroke(new BasicStroke(3.0f));
-	    g2d.draw(getEllipse());	    
-	}
-	if(getEsEstadoInicial()){
-	    g.setColor(DEFAULT_POINT_INITIAL_STATE);
-	    g2d.fill(getCirculoCinscunscritoEstadoInicial());
-	}
-	g.setColor(DEFAULT_FG_COLOR);
-	g2d.drawString(getLabel(),(int)(getX()+3),(int)(getY()+(DIAMETRO/2)));
-	g2d.setStroke(origS);
+        g.setColor(DEFAULT_BG_COLOR);
+        if (esEstadoActual==DEFAULT_CURRENT_STATE) {// Si no lo quieren raise entonces lo quieren m'as obscuro
+            g.setColor(DEFAULT_BG_CURRENT_STATE_COLOR);
+        }
+        g2d.fill(getEllipse());
+        if (getIsInF()) {
+            g.setColor(DEFAULT_FINAL_BG_COLOR);
+            g2d.setStroke(new BasicStroke(3.0f));
+            g2d.draw(getEllipse());
+        }
+        if (getEsEstadoInicial()) {
+            g.setColor(DEFAULT_POINT_INITIAL_STATE);
+            g2d.fill(getCirculoCinscunscritoEstadoInicial());
+        }
+        g.setColor(DEFAULT_FG_COLOR);
+        g2d.drawString(getLabel(),(int)(getX()+3),(int)(getY()+(DIAMETRO/2)));
+        g2d.setStroke(origS);
     }
 
     /**
      * Pinta un estado en un contexto gráfico con el background del
-     * color definido en <code>DEFAULT_BG_COLOR</code> 
+     * color definido en <code>DEFAULT_BG_COLOR</code>
      * @param g El contexto gráfico sobre el que se pintará el estado
      * @see #DEFAULT_BG_COLOR
      */
-    public void paint(Graphics g){
-	paint(g,!DEFAULT_CURRENT_STATE);
+    public void paint(Graphics g) {
+        paint(g,!DEFAULT_CURRENT_STATE);
     }
 
     /**
      * Regresa el centro del JState
      **/
-    public Point getCentro(){
-	Point tmp = new Point();
-	tmp.setLocation(e2d.getCenterX(),e2d.getCenterY());
-	return tmp;	
+    public Point getCentro() {
+        Point tmp = new Point();
+        tmp.setLocation(e2d.getCenterX(),e2d.getCenterY());
+        return tmp;
     }
 
-    public Point getQuadCurveP1(){
-	Point tmp = new Point();
-	tmp.setLocation((getX()+(DIAMETRO/4)),(getY()+(DIAMETRO/4)));
-	return tmp;	
+    public Point getQuadCurveP1() {
+        Point tmp = new Point();
+        tmp.setLocation((getX()+(DIAMETRO/4)),(getY()+(DIAMETRO/4)));
+        return tmp;
     }
 
-    public Point getQuadCurveP2(){
-	Point tmp = new Point();
-	tmp.setLocation((getX()+(DIAMETRO - DIAMETRO/4)),(getY()+(DIAMETRO/4)));
-	return tmp;	
+    public Point getQuadCurveP2() {
+        Point tmp = new Point();
+        tmp.setLocation((getX()+(DIAMETRO - DIAMETRO/4)),(getY()+(DIAMETRO/4)));
+        return tmp;
     }
 
-    public Point getQuadCurveCP(){
-	Point tmp = new Point();
-	tmp.setLocation((getX()+(DIAMETRO - DIAMETRO/4)),(getY() - DIAMETRO));	
-	return tmp;	
+    public Point getQuadCurveCP() {
+        Point tmp = new Point();
+        tmp.setLocation((getX()+(DIAMETRO - DIAMETRO/4)),(getY() - DIAMETRO));
+        return tmp;
     }
 
-    
-    public static void main(String argv[]){
-	
-	JState kk = new JState("HOLA", 5.5, 5.9);
-	Debug.println("\n"+kk+"\n");	
-    }
 
-    /**
-     *  Checa si un punto (x,y) esta en la circunferencia del JState
-     * @param x la coordenada en el eje X
-     * @param y la coordenada en el eje Y
-     */       
-    public boolean contains(int x, int y){
-	return contains((double)x, (double)y);
+    public static void main(String argv[]) {
+        JState kk = new JState("HOLA", 5.5, 5.9);
+        Debug.println("\n"+kk+"\n");
     }
 
     /**
      *  Checa si un punto (x,y) esta en la circunferencia del JState
      * @param x la coordenada en el eje X
      * @param y la coordenada en el eje Y
-     */       
-    public boolean contains(double x, double y){
-	return e2d.contains(x,y);
+     */
+    public boolean contains(int x, int y) {
+        return contains((double)x, (double)y);
     }
 
-    
+    /**
+     *  Checa si un punto (x,y) esta en la circunferencia del JState
+     * @param x la coordenada en el eje X
+     * @param y la coordenada en el eje Y
+     */
+    public boolean contains(double x, double y) {
+        return e2d.contains(x,y);
+    }
+
+
 } // JState
